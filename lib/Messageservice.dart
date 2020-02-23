174dd146d7422cd34608ecc40f6b413e976dd2d9
Peng Zhang <38088760+933039258@users.noreply.github.com>
2020-02-22 18:25:27 -0800

    Delete Messageservice.dart

--- lib/Messageservice.dart
+++
@@ -1,6 +0,0 @@
-import 'package:url_launcher/url_launcher.dart';
-
-class TelAndSmsService {
-  void sendSms(String number) => launch("sms:$number");
-  void sendEmail(String email) => launch("mailto:$email");
-}
